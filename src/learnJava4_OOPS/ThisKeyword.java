a31a20206e55d1b09755bf13685242734f82ae84
vikas <vikas@192.168.1.6>
2025-01-21 10:39:18 +0530

    This keyword description

--- src/learnJava4_OOPS/ThisKeyword.java
+++ src/learnJava4_OOPS/ThisKeyword.java
@@ -2,11 +2,21 @@ package learnJava4_OOPS;
 
 public class ThisKeyword {
 	
-	int x,y;    //x,y class variable or instance variable
-	ThisKeyword(int a,int b) //a,b are local variables and access within method
+	int x,y;    //x,y class variable or instance variable and can be accessed anywhere in 
+	
+	/*ThisKeyword(int a,int b) //a,b are local variables and access within method
 	{
-		x=a;
+		x=a;    
 		y=b;
+	}
+	*/
+	
+	ThisKeyword(int x,int y) //x,y are local variables and access within method
+	{
+		//x=x;   return output 0,if value is not specified
+		//y=y;   not able to understand which is class variable or local variable
+		this.x=x;  //
+		this.y=y;  
 	}
 	
 	void display()
@@ -20,7 +30,13 @@ public class ThisKeyword {
 		ThisKeyword var=new ThisKeyword(12,15);
 		var.display();
 
-		//Variables declared outside of class are called Global variable
+		/* Types of variables - local or class/instance
+		* Variables declared outside of class are called Global variable
+	    * not used in JAVA
+	    * this keyword always represent class/object
+	    * this keyword - to differentiate local variables and instance variables
+	    * this keyword used with constructor and Method also
+	    */
 	}
 
 }
