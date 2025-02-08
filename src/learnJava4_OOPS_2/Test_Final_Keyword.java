22fa414a8cb8eeaa7aa3c5ebad8f606da5162f88
vikas <vikas@192.168.1.6>
2025-02-09 02:16:00 +0530

    changes for keyword

--- src/learnJava4_OOPS_2/Test_Final_Keyword.java
+++ src/learnJava4_OOPS_2/Test_Final_Keyword.java
@@ -6,6 +6,44 @@ package learnJava4_OOPS_2;
 		int x=100;
 		final int y=500;
 	}
+// to use 'final' keyword in Method	
+	class Test2
+	{
+		final void view()
+		{
+			System.out.println("view method without final keyword");
+		}
+		
+	}
+	
+	class Test3 extends Test2
+	{
+	//	void view() //we cannot use view method since final Keyword is added before view method in Test2 class
+		{
+			System.out.println("this is samethod from class name as Test2");
+		}
+	}
+	
+//final keyword in class
+	
+	final class Test4
+	{
+		void displey()
+		{
+			System.out.println("using class with final keyword");
+		}
+	//class Test5 extends Test4  //child class cannot extends Test4 since final keyword is used
+	{
+		/*void displey()
+		{
+			System.out.println("child class cannot extends Test4 since final keyword is used");
+		}*/
+	}
+	
+	
+	}
+	
+	
 public class Test_Final_Keyword {
 
 	public static void main(String[] args) {
@@ -16,6 +54,12 @@ public class Test_Final_Keyword {
 		System.out.println(t.x);
 		System.out.println(t.y);
 
+/*variables - we cannot change the value of variables(constant)
+ * methods	- we cannot overrides those methods in child classes	
+ * class- we cannot extends that class into child class
+ */
+		
+		
 	}
 
 }
